2c0f0333de7b025a7940681aea6d45579851d256
Hyper <65469240+Hypersonic1054@users.noreply.github.com>
2025-12-18 12:10:56 -0500

    Create maze3.py

---
+++ maze3.py
@@ -0,0 +1,15 @@
+for step in range(1): # forward 3
+  move()
+
+for turn in range(3):
+  turn_left()
+
+for step in range(2):
+  move()
+  
+for turn in range(5):
+  turn_left()  
+
+for step in range(1):
+  move()
+  
